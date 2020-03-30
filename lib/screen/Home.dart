27421e96d9200056596b84847cdf6ce188163e13
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-03-31 00:15:58 +0530

    Added TabBar, Chat Screen

---
+++ lib/screen/Home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'Calls.dart';
+import 'Camera.dart';
+import 'Chat.dart';
+import 'Status.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 1, length: 4, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WhatsApp', style: TextStyle(color: Colors.white),),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.white,
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            color: Colors.white,
+            onPressed: (){},
+            ),
+        ],
+
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt, color: Colors.white,)),
+            Tab(child: Text("CHATS", style: Theme.of(context).textTheme.button)),
+            Tab(child: Text("STATUS", style: Theme.of(context).textTheme.button)),
+            Tab(child: Text("CALLS", style: Theme.of(context).textTheme.button)),
+          ],
+          ),
+
+      ),
+
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Camera(),
+          Chat(),
+          Status(),
+          Calls(),
+        ],
+        
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).accentColor,
+          onPressed: (){},
+          child: Icon(Icons.message, color: Colors.white),
+          ),
+    );
+  }
+}
