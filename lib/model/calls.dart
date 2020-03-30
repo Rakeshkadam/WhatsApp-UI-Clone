45f780050e421e8057199a0429bfa8d78b85bff9
Rakesh Kadam <rakesh22kadam@outlook.com>
2020-03-31 00:39:00 +0530

    Added Calls section

--- lib/model/calls.dart
+++ lib/model/calls.dart
@@ -31,5 +31,21 @@ List<calls> callData = [
       name: "Kaushal Jha",
       time: "11 April, 7:03 pm",
       imageUrl:
-          "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
+          "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+    new calls(
+      name: "Saksham Choudhary",
+      time: "10 April, 6:03 pm",
+      imageUrl:
+          "https://images.pexels.com/photos/1427288/pexels-photo-1427288.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+    new calls(
+      name: "Raushan Jha",
+      time: "10 April, 6:03 pm",
+      imageUrl:
+          "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new calls(
+      name: "Nitesh",
+      time: "14 April, 6:08pm",
+      imageUrl:
+          "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+
 ];
--- lib/screen/Calls.dart
+++ lib/screen/Calls.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mywhatsapp/model/chat.dart';
+import '../model/calls.dart';
 
 class Calls extends StatefulWidget {
   @override
@@ -8,8 +10,30 @@ class Calls extends StatefulWidget {
 class _CallsState extends State<Calls> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ListView.builder(
+      itemCount: callData.length,
+      itemBuilder: (context, i) =>
+        Column(
+          children: <Widget>[
+            Divider(height:20.0),
+            ListTile(
+              leading: CircleAvatar(
+                maxRadius: 25.0,
+                backgroundImage: NetworkImage(callData[i].imageUrl),
+              ),
+              title: Text(
+                callData[i].name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              subtitle: Container(
+                padding: EdgeInsets.only(top:5.0),
+                child: Text(callData[i].time, style: TextStyle(color: Colors.grey, fontSize:14.0),),
+              ),
+              trailing: Icon(Icons.call, color: Colors.green,),
+            ),
+          ],
+        )
     );
+  
   }
 }
